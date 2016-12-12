a7556b0d8b5a8a130253c428ac253c7da35b062d
Alan Berndt <alan@eatabrick.org>
2016-12-11 18:24:08 -0800

    Add draw_ex to plain sprites

--- sprite.cc
+++ sprite.cc
@@ -11,3 +11,10 @@ void Sprite::draw(Graphics& graphics, int x, int y) const {
   const SDL_Rect dest = {x, y, rect_.w, rect_.h };
   graphics.blit(file_, &rect_, &dest);
 }
+
+void Sprite::draw_ex(Graphics& graphics, int x, int y, bool flip, float angle, int cx, int cy) const {
+  SDL_Rect dest = { x, y, rect_.w, rect_.h };
+  SDL_Point center = { cx, cy };
+
+  graphics.blit_ex(file_, &rect_, &dest, angle, &center, flip ? Graphics::FlipDirection::HORIZONTAL : Graphics::FlipDirection::NONE);
+}
--- sprite.h
+++ sprite.h
@@ -11,6 +11,7 @@ class Sprite {
     Sprite(const std::string& file, int x, int y, int w, int h);
 
     virtual void draw(Graphics& graphics, int x, int y) const;
+    virtual void draw_ex(Graphics& graphics, int x, int y, bool flip, float angle, int cx, int cy) const;
 
   protected:
     SDL_Rect rect_;
