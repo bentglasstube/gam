fad449e47eac614b8528bb157642e5d0a35caa16
Alan Berndt <alan@eatabrick.org>
2020-04-21 13:58:31 -0700

    Add support for vertical parallax motion.

--- parallax_backdrop.cc
+++ parallax_backdrop.cc
@@ -3,20 +3,13 @@
 ParallaxBackdrop::ParallaxBackdrop(const std::string& file, int width, int height, double scale) :
   file_(file), width_(width), height_(height), scale_(scale) {}
 
-void ParallaxBackdrop::draw(Graphics& graphics, double xoffset, double yoffset) const {
+void ParallaxBackdrop::draw(Graphics& graphics, double xoffset, double) const {
   const SDL_Rect src = { 0, 0, width_, height_ };
   SDL_Rect dest = { 0, 0, width_, height_ };
 
-  dest.y = -yoffset / scale_;
-  while (dest.y > 0) dest.y -= height_;
-
-  while (dest.y < graphics.height()) {
-    dest.x = -xoffset / scale_;
-    while (dest.x > 0) dest.x -= width_;
-    while (dest.x < graphics.width()) {
-      graphics.blit(file_, &src, &dest);
-      dest.x += width_;
-    }
-    dest.y += height_;
+  dest.x = -xoffset / scale_;
+  while (dest.x < graphics.width()) {
+    graphics.blit(file_, &src, &dest);
+    dest.x += width_;
   }
 }
