4cb5660c06c891edffa8f6e4b0e67a51586ab8a2
Alan Berndt <alan@eatabrick.org>
2021-05-07 21:49:31 -0700

    Update parallax background to use new blit API.

--- parallax_backdrop.cc
+++ parallax_backdrop.cc
@@ -4,12 +4,12 @@ ParallaxBackdrop::ParallaxBackdrop(const std::string& file, int width, int heigh
   file_(file), width_(width), height_(height), scale_(scale) {}
 
 void ParallaxBackdrop::draw(Graphics& graphics, double xoffset, double) const {
-  const SDL_Rect src = { 0, 0, width_, height_ };
-  SDL_Rect dest = { 0, 0, width_, height_ };
+  const Graphics::Rect src = { 0, 0, width_, height_ };
+  Graphics::Rect dest = { 0, 0, width_, height_ };
 
   dest.x = -xoffset / scale_;
   while (dest.x < graphics.width()) {
-    graphics.blit(file_, &src, &dest);
+    graphics.blit(file_, src, dest);
     dest.x += width_;
   }
 }
